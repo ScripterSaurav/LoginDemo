279a07168e17e4bc8cdfdd74bd2786ed4db6ae1f
NσσႦ Sƈɾιρƚҽɾ <58369054+ScripterSaurav@users.noreply.github.com>
2023-05-28 00:30:43 +0530

    Create users.js

---
+++ users.js
@@ -0,0 +1,18 @@
+// User data
+var users = [
+  {
+    username: "admin",
+    password: "password",
+    active: true
+  },
+  {
+    username: "user1",
+    password: "123456",
+    active: false
+  },
+  {
+    username: "user2",
+    password: "abcdef",
+    active: true
+  }
+];
